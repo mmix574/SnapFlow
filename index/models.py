c3aa0831a7b7ccb029f94da596368887f17673e4
taita2015 <mmix.xia@gmail.com>
2017-02-26 23:59:53 +0800

    0226

--- index/models.py
+++ index/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 # 用户模型扩充
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    avatar_path = models.CharField(max_length=1024)
+    avatar_path = models.CharField(max_length=1024,default="")
     lang = models.CharField(max_length=10)
 
 
