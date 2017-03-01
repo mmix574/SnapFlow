379fe9947c87b0d39a2d7a24e0be27e6be33ca9b
taita2015 <mmix.xia@gmail.com>
2017-03-01 23:41:34 +0800

    0301

--- forum/models.py
+++ forum/models.py
@@ -21,3 +21,13 @@ class SubClass(models.Model):
     create_user = models.ForeignKey(User)
     create_time = models.DateTimeField(auto_now=True)
     last_time = models.DateTimeField(auto_now_add=True)
+
+
+class Thread(models.Model):
+    tittle = models.CharField(max_length=20)
+    content = models.TextField()
+    image = models.ImageField(null=True,blank=True)
+    create_user = models.ForeignKey(User)
+    last_time = models.DateTimeField(auto_now_add=True)
+    create_time = models.DateTimeField(auto_now=True)
+    pass
--- website/settings.py
+++ website/settings.py
@@ -126,9 +126,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'en_us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
