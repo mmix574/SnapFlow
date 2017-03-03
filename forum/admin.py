f95e148e6b4e25deac87dcb272e048bd42f3cb73
taita2015 <mmix.xia@gmail.com>
2017-03-03 13:42:46 +0800

    0303

--- forum/admin.py
+++ forum/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Class
-from .models import SubClass
-
-from .models import Thread
-
-
-admin.site.register(Class)
-admin.site.register(SubClass)
-# admin.site.register(Thread)
--- forum/forms.py
+++ forum/forms.py
@@ -1,9 +1,1 @@
-from django import forms
-from .models import Class
-
-class ClassForm(forms.ModelForm):
-    class Meta:
-        model=Class
-        fields = []
-
 
