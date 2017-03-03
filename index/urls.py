196c0a26926c6eecd86c1402c88d1d4555432be4
taita2015 <mmix.xia@gmail.com>
2017-03-03 20:07:38 +0800

    0303

--- index/urls.py
+++ index/urls.py
@@ -4,16 +4,24 @@ from . import views
 from . import v2views
 
 urlpatterns = [
+    # 欢迎界面，包括未登录欢迎界面和登陆之后的欢迎界面
     url(r'^$',views.IndexView.as_view()),
+    # 提示消息
     url(r'message',views.MessageView.as_view()),
-
+    # 登陆
     url(r'^login$',views.login),
+    # 注册
     url(r'^register$',views.register),
-    url(r'^about$',views.about),
+    # 登出
+    url(r'^logout$', views.LogoutView.as_view()),
+
+    # 测试
     url(r'^username$',views.username),
-    url(r'^logout$',views.LogoutView.as_view()),
     url(r'^test',views.TestView.as_view()),
+
     url(r'^passwordchange', views.PasswordChangeView.as_view()),
     url(r'^none$',views.NoneView.as_view()),
+
+    # 2017年3月3日 20:07:22
     url(r'^w', v2views.V2Index.as_view()),
 ]
