58e3c4ac4147f12c34e5bbea0f47d26f7cccf729
taita2015 <mmix.xia@gmail.com>
2017-03-04 10:13:27 +0800

    0304

--- index/models.py
+++ index/models.py
@@ -37,6 +37,7 @@ class UserProfile(models.Model):
     avatar = models.ImageField(null=True,blank=True,upload_to=upload_to)
     set_avatar = models.BooleanField(default=False)
     # avatar = models.ImageField(null=True,blank=True)
+    work_year = models.IntegerField(default=0)
     work_place = models.CharField(max_length=20)
     work_nickname = models.CharField(max_length=20)
     language = models.CharField(max_length=10,null=True,blank=True)
