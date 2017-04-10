c2cf2e08d98dbbdaa2f8811f3e5565865d78ce6e
taita2015 <mmix.xia@gmail.com>
2017-04-10 08:44:35 +0800

    0410

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import SystemToUserMessage
+
+admin.site.register(SystemToUserMessage)
--- message/models.py
+++ message/models.py
@@ -13,8 +13,12 @@ class SystemToUserMessage(models.Model):
     user = models.ForeignKey(User,default=1)
     content = models.CharField(max_length=500,null=True,blank=True)
     read = models.BooleanField(default=False)
-
     time = models.TimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "系统消息"
+        verbose_name_plural = verbose_name
+        pass
     pass
 
 # @ 回复等，点赞等
