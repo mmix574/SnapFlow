f3b31e16e32e5bfc021a2d4bbd2f916925cadc4d
taita2015 <mmix.xia@gmail.com>
2017-04-12 16:34:44 +0800

    0412

--- space/urls.py
+++ space/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^member/$', views.MemberView.as_view()),
     url(r'^userdata/$', views.UserDataView.as_view(),name="userdata"),
 ]
--- space/views.py
+++ space/views.py
@@ -25,36 +25,7 @@ from .forms import UserForm
 from website.utils import console
 from website.utils import messages
 
-@method_decorator(login_required,name='dispatch')
-class MemberView(AppBaseTemplateView):
-    template_name = "space/member.html"
 
-    def get(self, request, *args, **kwargs):
-        up = UserProfile.objects.get(user=request.user)
-        form = UserProfileForm(instance=up)
-        return super(MemberView, self).get(request,context={"form":form},*args,**kwargs)
-
-
-    def post(self,request,*args,**kwargs):
-        up = UserProfile.objects.get(user=request.user)
-        from website.utils import console
-        # console.log(request.POST)
-        # console.log(request.FILES)
-
-        form = UserProfileForm(request.POST,request.FILES,instance=request.user.userprofile)
-        # model = form.save(commit=False)
-        # model.user = request.user
-        # model.id = up.id
-        # model.save()
-        #
-        # up = UserProfile.objects.get(user=request.user)
-        # form = UserProfileForm(instance=up)
-        if form.is_valid():
-            form.save()
-
-        form = UserProfileForm(instance=request.user.userprofile)
-
-        return super(MemberView, self).post(request,context={"form":form},*args,**kwargs)
 
 
 
@@ -73,43 +44,13 @@ class UserDataView(AppBaseTemplateView):
 
     def post(self,request,context={},*args,**kwargs):
 
-        form1 = UserForm(instance=request.user,data=request.POST)
-        form2 = UserProfileForm(request.POST,request.FILES,instance=request.user.userprofile)
-        form3 = UserAvatarForm(request.POST,request.FILES,instance=request.user.userprofile)
-        # form1 = UserForm(instance=request.user)
-        # form2 = UserProfileForm(instance=request.user.userprofile)
-        # form3 = UserAvatarForm(instance=request.user.userprofile)
 
-        tab_name = request.POST.get("tab_name","tab1")
-
-        if tab_name=="tab1":
-            if form1.is_valid():
-                form1.save()
-            else:
-                console.log("form1 invalidate")
-            pass
-            if form2.is_valid():
-                form2.save()
-            else:
-                console.log("form2 invalidate")
-            pass
-
-        if tab_name=="tab2":
-            if form3.is_valid():
-                form3.save()
-                form3 = UserAvatarForm(instance=request.user.userprofile)
-            else:
-                console.log("form3 invalidate")
-            pass
-        if tab_name=="tab3":
-            pass
+        user_form = UserForm(instance=request.user)
+        user_profile_form = UserProfileForm(instance=request.user.userprofile)
+        user_avatar_form = UserAvatarForm(instance=request.user.userprofile)
 
 
-        form1 = UserForm(instance=request.user)
-        form2 = UserProfileForm(instance=request.user.userprofile)
-        form3 = UserAvatarForm(instance=request.user.userprofile)
-
-        return super(UserDataView, self).post(request,context={"form1":form1,"form2":form2,"form3":form3,"tab_name":tab_name},*args,**kwargs)
+        return super(UserDataView, self).post(request,context={"user_form":user_form,"user_profile_form":user_profile_form,"user_avatar_form":user_avatar_form},*args,**kwargs)
 
 
 class UserHomePageView(AppBaseTemplateView):
