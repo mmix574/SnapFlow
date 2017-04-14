866a45fe7e709bed16f0994fcef0170e53cbd543
taita2015 <mmix.xia@gmail.com>
2017-04-15 00:58:48 +0800

    0415

--- fake/management/commands/fakeusers.py
+++ fake/management/commands/fakeusers.py
@@ -3,10 +3,15 @@ from django.contrib.auth.models import User
 import random
 
 
+from django.contrib.auth.models import User
+from index.models import UserProfile
+
 def get_random_username():
+    start_with = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@/./+/-/_"
     sa = []
-    for i in range(8):
+    sa.append(random.choice(start_with))
+    for i in range(7):
         sa.append(random.choice(seed))
     salt = ''.join(sa)
     return salt
@@ -20,5 +25,26 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("creating fake users")
         # print(User.objects.all())
-        # u = User()
-        print(get_random_username())
+
+        _name = get_random_username()
+
+        #
+        u = User()
+        u.username = _name
+        u.email = _name+"@gmail.com"
+        u.save()
+
+        #
+        # create_user = User.objects.filter(username=_name)
+        # if not create_user:
+        #     return
+
+        # user = create_user[0]
+        u.userprofile.language = "english"
+        u.work_place = "google"
+        u.userprofile.work_nickname = "hacker-painter"
+        u.userprofile.self_introduction = "もしも彼らが君の何かを盗んだとして それはくだらないものだよ  返して贳うまでもない筈甚至何故なら価値は生命に従って付いている"
+
+        u.userprofile.avatar="ava/2_4ydFf85.jpg"
+
+        u.userprofile.save()
