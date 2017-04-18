b7b929a9c8a612984804f8c665bc33147f3413b7
taita2015 <mmix.xia@gmail.com>
2017-04-18 23:40:14 +0800

    0418

--- history/models.py
+++ history/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class History(models.Model):
-    type_chioces = (("asking","提问"),("answering","回答"),("liking","点赞"),("collecting","收藏"))
+    type_chioces = (("asking","提问"),("answering","回答"),("liking","点赞"),("collecting","收藏"),("commenting","回答"))
 
     user = models.ForeignKey(User)
     type = models.CharField(max_length=20,choices=type_chioces)
@@ -13,13 +13,19 @@ class History(models.Model):
     url = models.URLField(blank=True)
     create_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.user.username+"-->"+self.type+":"+self.brief_content
+
     class Meta:
         verbose_name = "历史"
         verbose_name_plural = verbose_name
 
+
+
 # event receive
 
 from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 
@@ -32,3 +38,43 @@ def add_like(sender,instance,created,**kwargs):
         h.user = instance.user
         h.brief_content = instance.thread.tittle
         h.save()
+
+from forum.models import Thread
+@receiver(post_save,sender=Thread)
+def add_thread_history(sender,instance,created,**kwargs):
+    if created:
+        h = History()
+        h.type = "asking"
+        h.brief_content = instance.tittle
+        h.user = instance.create_user
+        h.save()
+
+from forum.models import Comment
+@receiver(post_save,sender=Comment)
+def add_comment_history(sender,instance,created,**kwargs):
+    if created:
+        h = History()
+        h.type = "commenting"
+        h.brief_content = instance.content
+        h.user = instance.create_user
+        h.save()
+
+from collection.models import Collection
+@receiver(post_save,sender=Collection)
+def add_collection_history(sender,instance,created,**kwargs):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- history/views.py
+++ history/views.py
@@ -8,11 +8,23 @@ from django.utils.decorators import method_decorator
 
 from django.http.response import JsonResponse
 
+from .models import History
+
 
 @method_decorator(login_required,name='dispatch')
 class HistoryView(AppBaseTemplateView):
     template_name = 'history/history.html'
-    pass
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        history_list = History.objects.filter(user=self.request.user).order_by('-create_time')
+        context['history_list'] = history_list
+        return context
+    def get(self, request, context={}, *args, **kwargs):
+        return super().get(request, context, *args, **kwargs)
+
+    def post(self, request, context={}, *args, **kwargs):
+        return super().post(request, context, *args, **kwargs)
 
 
 def add_history(request):
