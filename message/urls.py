d096dd2be5b8a8dabf0fe0900ca10baab434d7fb
taita2015 <mmix.xia@gmail.com>
2017-04-21 16:03:15 +0800

    0421

--- message/urls.py
+++ message/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(),name='_messaging'),
-    url(r'^system-message$',views.SystemMessageView.as_view(),name='system_message'),
-    url(r'^private-message$', views.PrivateMessageView.as_view(), name='private_message'),
-    url(r'^message-status$', views.MessageStatusView.as_view(), name='message_status'),
-    url(r'^event-message$', views.EventMessageView.as_view(), name='event_message'),
-    url(r'^friend$', views.FriendView.as_view(), name='space-friend'),
+    url(r'^system-message/$',views.SystemMessageView.as_view(),name='system_message'),
+    url(r'^private-message/$', views.PrivateMessageView.as_view(), name='private_message'),
+    url(r'^message-status/$', views.MessageStatusView.as_view(), name='message_status'),
+    url(r'^event-message/$', views.EventMessageView.as_view(), name='event_message'),
+    url(r'^friend/$', views.FriendView.as_view(), name='space-friend'),
 ]
